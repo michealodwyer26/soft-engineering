4ed2d5921b6b4ca51b7b2ce8b4e644166b0e7d54
Naphtali Odinakachi <48455262+NaphtaliO@users.noreply.github.com>
2022-10-28 13:00:41 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,38 @@
+class godModeController:
+    pass
+
+class Agent:
+    def listenForGodMode(self):
+        pass
+
+    def listenForResponse(self):
+        pass
+
+class coreController:
+    def listenForGodMode(self):
+        pass
+
+    def visualise(self):
+        pass
+
+    def createBot(self):
+        pass
+
+    def deleteBot(self):
+        pass
+
+class sentimentController:
+    def scraping(self):
+        pass
+
+        #Processing the data
+    def process(self):
+        pass
+
+    def databaseTransfer(self):
+        pass
+
+
+class Visualiser:
+    pass
+
