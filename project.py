928dd0d79dfd220212cb3bc616f44e3557042c70
Naphtali Odinakachi <48455262+NaphtaliO@users.noreply.github.com>
2022-11-01 16:59:24 +0000

    Created more methods and a scraping script for twitter

--- project.py
+++ project.py
@@ -1,24 +1,21 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+
 class godModeController:
-    
     def __init__(self):
         pass
 
+    def visualiserInputListener(self):
+        pass
+
+    def action(self):
+        pass
+
+
 class Agent:
-
-    def __init__(self, balance):
-        self._balance = balance
-
-    def setBalance(self, balance):
-        self._balance = balance
-        return
-    
-    def addBalance(self, balance):
-        self._balance += balance
-        return
-
-    def subtractBalance(self, balance):
-        self._balance -= balance
-        return
+    def __init__(self):
+        pass
 
     def listenForGodMode(self):
         pass
@@ -26,8 +23,18 @@ class Agent:
     def listenForResponse(self):
         pass
 
-class coreController:
+    def investingState(self):
+        pass
 
+    # Taking Earnings & Sending Feedback
+    def feedback(self):
+        pass
+
+    def die(self):
+        pass
+
+
+class coreController:
     def __init__(self):
         pass
 
@@ -43,16 +50,32 @@ class coreController:
     def deleteBot(self):
         pass
 
-class sentimentController:
 
+class sentimentController:
     def __init__(self):
         pass
+    
+    # Handles data processing and databse Transfer
+    def scraping(self):      
+        query = "crypto"
+        tweets = []
+        limit = 200
 
-    def scraping(self):
-        pass
+        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+            if len(tweets) == limit:
+                break
+            else:
+                tweets.append([tweet.date, tweet.user.username, tweet.content])
 
-        #Processing the data
-    def process(self):
+        df = pd.DataFrame(tweets, columns=["Date", "User", "Tweet"])
+        print(df)
+
+        # saves to csv file
+        # df.to_csv('tweets.csv')
+
+    def requestListener(self):
+        #API request --> API response
+        #Sentiment request --> Sentiment response
         pass
 
     def databaseTransfer(self):
@@ -60,6 +83,17 @@ class sentimentController:
 
 
 class Visualiser:
-
     def __init__(self):
+        pass
+
+    def listener(self):
+        pass
+
+    def godeModeAction():
+        pass
+
+    def coreControllerFeedbackReceiver():
+        pass
+
+    def visualise(self):
         pass
