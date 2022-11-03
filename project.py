9a873172911597d99c4b04428fb2cdbd7deea8a9
Ciarán Cotter <74498922+ciarancotter@users.noreply.github.com>
2022-11-03 09:51:21 +0000

    Update project.py
    Updated project.py to scrape 500,000 tweets and enter them into the MySQL database.

--- project.py
+++ project.py
@@ -1,5 +1,12 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import sqlalchemy as db
+
+try:
+  engine = db.create_engine("mysql+pymysql://softwareuser:$B4s3dcrypt0$@localhost/project")
+  print("Connection successful")
+except Exception as e:
+  print(e)
 
 
 class godModeController:
@@ -66,24 +73,26 @@ class coreController:
 class sentimentController:
     def __init__(self):
         pass
-    
+
     # Handles data processing and databse Transfer
-    def scraping(self):      
-        query = "crypto"
+    def scraping(self):
+        query = "#crypto"
         tweets = []
-        limit = 200
+        limit = 200 # was 500,000
+        count = 0
 
         for tweet in sntwitter.TwitterSearchScraper(query).get_items():
             if len(tweets) == limit:
+                print("Done!")
                 break
             else:
-                tweets.append([tweet.date, tweet.user.username, tweet.content])
+                count += 1
+                print(count)
+                tweets.append([tweet.date, tweet.content])
 
-        df = pd.DataFrame(tweets, columns=["Date", "User", "Tweet"])
-        print(df)
-
-        # saves to csv file
-        # df.to_csv('tweets.csv')
+        df = pd.DataFrame(tweets, columns=["Date", "Tweet"])
+        #print("Dataframe transfer complete!")
+        #df.to_sql("Tweets", engine, if_exists="replace", index=False)
 
     def requestListener(self):
         #API request --> API response
@@ -109,3 +118,6 @@ class Visualiser:
 
     def visualise(self):
         pass
+
+sentiment = sentimentController()
+sentiment.scraping()
