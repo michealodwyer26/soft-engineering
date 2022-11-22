e337b0dfa3a53c7a93ed0bf260c205e2f1bf7d59
ciarancotter <120326406@umail.ucc.ie>
2022-11-22 18:31:47 +0000

    Started developing the demo application.

---
+++ colony_client/demo.py
@@ -0,0 +1,6 @@
+# The demo application for a core controller.
+
+from colony_client.bot import Bot
+from colony_client.core_controller import CoreController
+
+testCoreController = CoreController("demo")
