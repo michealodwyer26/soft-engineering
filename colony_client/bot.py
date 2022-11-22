7ceda8112f1dba5b6b42a6982fa41778514fe3d8
Schuch <eoin.schuch.5@gmail.com>
2022-11-22 14:29:13 +0000

    Added new attributes and changed bots constructor.

--- colony_client/bot.py
+++ colony_client/bot.py
@@ -1,16 +1,25 @@
 from custom_logger import Logger
+import requests
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
 
 class Bot:
-    def __init__(self, identifier, balance):
+    def __init__(self, identifier, coinBalance, balance, coin):
         self.identifier = identifier
+        self._coinBalance = coinBalance
         self._balance = balance
+        self._initialBalance = balance
         self.xpos = 0
         self.ypos = 0
         self.logger = Logger()
         self.logTitle = "bot"
+        self.coin = coin
+        self.engine = None
 
     def getBalance(self):
         return self._balance
+
     def setBalance(self, balance):
         self._balance = balance
         return
