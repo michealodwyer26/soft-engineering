556aa18f94ae6a660768d052f72371ab39136a11
Naphtali <naphtali2003@gmail.com>
2022-11-23 01:21:04 +0000

    Updated Visualiser

--- colony_client/visualiser.py
+++ colony_client/visualiser.py
@@ -1,20 +1,68 @@
 from .bot import Bot
 from .core_controller import CoreController
+import pygame
+import random
+
+# class Visualiser:
+#     def __init__(self):
+#         self.mainDisplay = None
+#         self.logTitle = "visualiser"
+
+#     def initialise(self):
+#         screen = (800, 600)
+#         pg.init()
+#         pg.display.set_caption('Crypto Bot Colony')
+#         self.mainDisplay = pg.display.set_mode(screen)
+#         displayer = pg.Surface(screen)
+
+#     def drawBot(self, bot):
+#         self.mainDisplay.set_at((bot.xpos, bot.ypos), (255, 255, 255))
+
+#     def eraseBot(self, bot):
+#         self.mainDisplay.set_at((bot.xpos, bot.ypos), (0, 0, 0))
 
 class Visualiser:
     def __init__(self):
-        self.mainDisplay = None
-        self.logTitle = "visualiser"
+        self.WIDTH, self.HEIGHT = 500, 500
+        self.WIN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Visualization")  # window name
+        self.WHITE = (255, 255, 255)
+        self.FPS = 10
+        self.main()
 
-    def initialise(self):
-        screen = (800, 600)
-        pg.init()
-        pg.display.set_caption('Crypto Bot Colony')
-        self.mainDisplay = pg.display.set_mode(screen)
-        displayer = pg.Surface(screen)
+    def draw_window(self):
+        self.WIN.fill(self.WHITE)
+        pygame.draw.circle(self.WIN, (0, 255, 0), (250, 250), 30)
+        self.draw_bot()
+        pygame.display.update()
 
-    def drawBot(self, bot):
-        self.mainDisplay.set_at((bot.xpos, bot.ypos), (255, 255, 255))
+    # def draw_bot(self):
+    #     bot_coordinate = (100, 100)
+    #     surf = pygame.Surface((50, 50))
+    #     surf.fill((0, 0, 0))
+    #     rect = surf.get_rect()
+    #     self.WIN.blit(surf, (self.WIDTH/2, self.HEIGHT/2))
+    #     pygame.display.flip()
 
-    def eraseBot(self, bot):
-        self.mainDisplay.set_at((bot.xpos, bot.ypos), (0, 0, 0))
+    def draw_bot(self):
+        bot_coordinate = (random.randint(0, 500), random.randint(0, 500))
+        # bot = pygame.draw.circle(self.WIN, (0, 0, 0), bot_coordinate, 10)
+        bot = pygame.Surface((10, 10))
+        bot.fill((0, 0, 0))
+        rect = bot.get_rect()
+        self.WIN.blit(bot, bot_coordinate)
+        pygame.display.flip()
+
+    def main(self):
+        clock = pygame.time.Clock()
+        run = True
+        while run:
+            clock.tick(self.FPS)  # Never go over 60 fps
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            self.draw_window()
+        pygame.quit()
+
+
+vis = Visualiser()
